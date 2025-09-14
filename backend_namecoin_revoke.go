6b1cb3a5d19b75878ed7c0818396c00610a8fc8b
Jeremy Rand <jeremyrand@danwin1210.de>
2025-09-14 22:11:49 +0000

    FIXUP comma

--- backend_namecoin_revoke.go
+++ backend_namecoin_revoke.go
@@ -150,7 +150,7 @@ func (b *BackendNamecoinRevoke) QueryIssuerSerial(issuer *pkix.Name, serial *big
 		BasicConstraintsValid: true,
 
 		// TODO: calculate this properly
-		SubjectKeyId: []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}
+		SubjectKeyId: []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31},
 	}
 
 	crlBytes, err := x509.CreateRevocationList(rand.Reader, crlTemplate, crlIssuer, priv)
